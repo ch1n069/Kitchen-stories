c6e4f1d487503c0b0b154db5c7c2716b156ed1fb
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-30 01:39:26 +0300

    created the navabar of the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Nav } from "react-bootstrap";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Nav></Nav>
+    </div>
+  );
 }
 
 export default App;
