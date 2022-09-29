4c2f0850262ecef49666778e034a4293b3c0cfc9
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-30 02:22:18 +0300

    added the api for the popular recipes

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,8 +1,16 @@
+import React, { useEffect } from "react";
+
 function Popular() {
+  useEffect(() => {
+    getPopular();
+  }, []);
+
   const getPopular = async () => {
     const api = await fetch(
-      ` https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+      ` https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
+    const data = await api.json();
+    console.log(data);
   };
   return <div>popular</div>;
 }
