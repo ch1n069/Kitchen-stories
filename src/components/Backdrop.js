6bd3035ac3c6a1a2539d1e45fde96f78367773d4
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-30 20:19:08 +0300

    ft === finalized on the welcome backdrop feauture

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Backdrop.css";
 function Backdrop() {
+  // custom functions
+  const [showing, setToHidden] = useState(true);
+
+  // functions are above
+
+  const hideHandler = () => {
+    console.log("you clicked me");
+    setToHidden(false);
+  };
+  // functions go above
   return (
-    <div className="body">
-      <div className="chef">
-        <h2>Whip up Your fav meal</h2>
-        <button className="btn btn-primary button" type="">
-          Get recipe
-        </button>
-      </div>
+    <div>
+      {showing ? (
+        <div onClick={hideHandler} className="body">
+          <div className="chef">
+            <h2>Whip up Your fav meal</h2>
+            <button className="btn btn-primary button" type="">
+              Get recipe
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
